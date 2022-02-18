4658d25ad24462136e536a223f3f800821d07460
Shivam Sharma <shivam.piyush99@gmail.com>
2022-02-18 13:55:28 +0530

    This is my third program in git hub

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Number: ");
+            int num = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("This number is prime no.");
+            }
+            else
+            {
+                Console.WriteLine("This number is not prime");
+            }
+            Console.ReadLine();
+        }
+    }
+}
